69d1e0a6d49dd15feffcc84b5070e86253135a84
wolfgang <wmhuber2@illinois.edu>
2019-11-13 09:28:36 -0600

    Sailing Fix

--- Map.py
+++ Map.py
@@ -2416,13 +2416,9 @@ async def updateInAnnouncements(server, reload=True, postToSpam = False):
                             itemDelta[itm] = {'-': 0.0, '+': 0.0}
                         itemDelta[itm]['+'] += float(1) * modifier
 
-                    if name == 'saiboat' and Data[guild]['Players'][player]['Markers']['Properties'][tileIndex] \
+                    if unit == 'saiboat' and Data[guild]['Players'][player]['Markers']['Properties'][tileIndex] \
                             ['Unit'].get('MobileMoveCount') in [0, None]:
                         itm  = 'Oil'
-                        if itm == None:
-                            itm = random.choice(rawMaterialsList)
-                            Data[guild]['Players'][player]['Markers']['Properties'][tileIndex]['Unit'][
-                                'VillageItem'] = itm
                         if itemDelta.get(itm) is None:
                             itemDelta[itm] = {'-': 0.0, '+': 0.0}
                         itemDelta[itm]['+'] += float(2) * modifier
